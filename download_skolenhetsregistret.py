033b46b0a153587d84f4ef12812ca3c856d66fd2
rotsee <mejl@leowallentin.se>
2020-08-25 18:56:49 +0200

    add more dates

--- download_skolenhetsregistret.py
+++ download_skolenhetsregistret.py
@@ -11,21 +11,14 @@ import json
 
 BASE = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/skolenhet"
 DATES = [
-    "20200101",
-    "20190701",
-    "20190101",
-    "20180701",
-    "20180101",
-    "20170701",
-    "20170101",
-    "20160701",
-    "20160101",
-    "20150701",
-    "20150101",
-    "20140701",
-    "20140101",
-    "20130701",
-    "20130101",
+    "20200701", "20200401", "20200101",
+    "20191001", "20190701", "20190401", "20190101",
+    "20181001", "20180701", "20180401", "20180101",
+    "20171001", "20170701", "20170401", "20170101",
+    "20161001", "20160701", "20160401", "20160101",
+    "20151001", "20150701", "20150401", "20150101",
+    "20141001", "20140701", "20140401", "20140101",
+    "20131001", "20130701", "20130401", "20130101",
 ]
 # fetch list of schools
 d = requests.get(BASE).json()
@@ -43,10 +36,11 @@ with open(f"{DATA_DIR}/skolenheter.csv", "w") as file_:
     writer.writerows(d["Skolenheter"])
 
 # Fetch data for each school, at numerous times
-session = boto3.Session()
+session = boto3.Session()  # profile_name="XXX"
 s3_client = session.client("s3")
 for school in d["Skolenheter"]:
     id_ = school['Skolenhetskod']
+    print(f"Downloading {school['Skolenhetsnamn']} ({id_})")
     for date in DATES:
         endpoint = f"{BASE}/{id_}/{date}"
         d = requests.get(endpoint).json()
